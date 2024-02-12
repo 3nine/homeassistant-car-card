8892fa3e21684c79ea0576e9a126dbdc12980cfb
hxnning <106799256+3nine@users.noreply.github.com>
2024-02-12 07:35:14 +0100

    Update car-card-config.js

--- src/cards/car-card/car-card-config.js
+++ src/cards/car-card/car-card-config.js
@@ -1,35 +1,60 @@
 class CarCard extends HTMLElement {
-    set hass(hass) {
-        if (!this.content) {
-            const cardConfig = this._config;
-            this.content = document.createElement('div');
-            this.content.innerHTML = `
+    setConfig(config) {
+        if (!config || !config.cards || !Array.isArray(config.cards)) {
+            throw new Error('Invalid configuration');
+        }
+        
+        this.config = config;
+        this.render();
+    }
+
+    render() {
+        const cards = this.config.cards.map(card => {
+            return `
                 <ha-card>
-                    <img src="${cardConfig.background}" style="width: 100%; height: auto;">
-                    <div style="display: flex; justify-content: space-between;">
-                        ${this.createEntities(cardConfig.top_entities)}
-                    </div>
-                    <div style="display: flex; justify-content: space-between;">
-                        ${this.createEntities(cardConfig.bottom_entities)}
-                    </div>
+                    ${this.generateCard(card)}
                 </ha-card>
             `;
-            this.appendChild(this.content);
-        }
-    }
+        }).join('');
 
-    setConfig(config) {
-        this._config = config;
-    }
-
-    createEntities(entities) {
-        return entities.map(entity => `
-            <div>
-                <ha-icon icon="${entity.icon}"></ha-icon>
-                <span>${entity.name}</span>
+        this.innerHTML = `
+            <div class="custom-card">
+                ${cards}
             </div>
-        `).join('');
+        `;
+    }
+
+    generateCard(card) {
+        const type = card.type;
+
+        if (type === 'conditional') {
+            const conditions = card.conditions;
+            const state = conditions[0].entity.split('.')[0];
+            const stateValue = conditions[0].state || 'on';
+
+            if (this.hass.states[state].state !== stateValue) {
+                return '';
+            }
+        }
+
+        if (type === 'picture-elements') {
+            return `
+                <img src="${card.image}" style="width: 100%; height: auto;">
+                ${card.elements.map(element => `
+                    <div style="position: absolute; top: ${element.style.top}; left: ${element.style.left};">
+                        <ha-icon icon="${element.entity.icon || ''}"></ha-icon>
+                        <span>${element.entity.name || ''}</span>
+                    </div>
+                `).join('')}
+            `;
+        }
+
+        return '';
+    }
+
+    set hass(hass) {
+        this.hass = hass;
+        this.render();
     }
 }
-
 customElements.define('car-card', CarCard);
