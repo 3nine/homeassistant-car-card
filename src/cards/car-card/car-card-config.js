cbd9163eef1f47542334fb2c23c62c79e05d51e1
hxnning <106799256+3nine@users.noreply.github.com>
2024-02-12 07:27:49 +0100

    Update and rename car-card-config to car-card-config.js

---
+++ src/cards/car-card/car-card-config.js
@@ -0,0 +1,35 @@
+class CustomCard extends HTMLElement {
+    set hass(hass) {
+        if (!this.content) {
+            const cardConfig = this._config;
+            this.content = document.createElement('div');
+            this.content.innerHTML = `
+                <ha-card>
+                    <img src="${cardConfig.background}" style="width: 100%; height: auto;">
+                    <div style="display: flex; justify-content: space-between;">
+                        ${this.createEntities(cardConfig.top_entities)}
+                    </div>
+                    <div style="display: flex; justify-content: space-between;">
+                        ${this.createEntities(cardConfig.bottom_entities)}
+                    </div>
+                </ha-card>
+            `;
+            this.appendChild(this.content);
+        }
+    }
+
+    setConfig(config) {
+        this._config = config;
+    }
+
+    createEntities(entities) {
+        return entities.map(entity => `
+            <div>
+                <ha-icon icon="${entity.icon}"></ha-icon>
+                <span>${entity.name}</span>
+            </div>
+        `).join('');
+    }
+}
+
+customElements.define('custom-card', CustomCard);
